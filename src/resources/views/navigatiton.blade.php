a2b0a4ef25233c2baa65db22074fe2ead1fb4b5f
keita kato <infotec62509@gmail.com>
2026-01-05 00:31:58 +0900

    必要に応じてヘッダーに表示するnavigation.blade.phpを作成

---
+++ src/resources/views/navigatiton.blade.php
@@ -0,0 +1,7 @@
+<form action="/" method="get" class="search-form">
+    <!-- TODO：今開いているのがおすすめなのかマイリストなのかを保持するためにデータを送る必要がある(defaultはおすすめ) -->
+    <input type="text" class="search-form__input">
+</form>
+<nav>
+
+</nav>
