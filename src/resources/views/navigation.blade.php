df7963a84378b0c12777f9774883f08c9a2037d9
keita kato <infotec62509@gmail.com>
2026-01-05 17:50:14 +0900

    共通ヘッダー部分のスタイルを作成

--- src/resources/views/navigation.blade.php
+++ src/resources/views/navigation.blade.php
@@ -1,7 +1,37 @@
 <form action="/" method="get" class="search-form">
     <!-- TODO：今開いているのがおすすめなのかマイリストなのかを保持するためにデータを送る必要がある(defaultはおすすめ) -->
-    <input type="text" class="search-form__input">
+    <input type="text" class="search-form__input" placeholder="なにをお探しですか？">
 </form>
 <nav>
-
+    <ul class="header-navigation">
+        @auth
+        <li>
+            <form action="/logout" method="post">
+                @csrf
+                <button class="header-navigation__logout">
+                    ログアウト
+                </button>
+            </form>
+        </li>
+        @endauth
+        @guest
+        <li>
+            <form action="/login" method="post">
+                <button class="header-navigation__login">
+                    ログイン
+                </button>
+            </form>
+        </li>
+        @endguest
+        <li>
+            <a href="/mypage" class="header-navigation__mypage">
+                マイページ
+            </a>
+        </li>
+        <li>
+            <a href="/sell" class="header-navigation__sell">
+                出品
+            </a>
+        </li>
+    </ul>
 </nav>
