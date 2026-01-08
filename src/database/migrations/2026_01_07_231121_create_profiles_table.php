851eae80bd2df27660564faadfa0e1f10b012ddf
keita kato <infotec62509@gmail.com>
2026-01-08 12:15:09 +0900

    マイグレーションの作成

--- src/database/migrations/2026_01_07_231121_create_profiles_table.php
+++ src/database/migrations/2026_01_07_231121_create_profiles_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('image')->nullable();
+            $table->string('postal_code');
+            $table->string('address');
+            $table->string('building');
             $table->timestamps();
         });
     }
