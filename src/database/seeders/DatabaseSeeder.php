c1dadd76bb571fc210dbd6f5cec5baa0bcc9e1c5
keita kato <infotec62509@gmail.com>
2026-01-11 01:10:47 +0900

    シーディング実行時に発生していたエラーの原因を取り除きました。

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
 
-        $this->call(ItemSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ConditionSeeder::class);
+        $this->call(ItemSeeder::class);
     }
 }
