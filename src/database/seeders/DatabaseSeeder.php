645ebf2af5049e103dd026dc03dc1e6542675dca
keita kato <infotec62509@gmail.com>
2026-01-11 00:13:40 +0900

    シーダーを実行するための変更を行いました。

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(ItemSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ConditionSeeder::class);
     }
 }
