2ff5b663390f635aeec5a0cb8692e85e4513f157
keita kato <infotec62509@gmail.com>
2026-01-19 01:31:59 +0900

    新規会員登録ページのviewファイル作成

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+@section('content')
+<div class="register-page">
+    <div class="register-page__header">
+        <h2 class="register-page__header-title">会員登録</h2>
+    </div>
+    <form action="/register" method="post" class="register-form" novalidate>
+        <label class="register-form__label">ユーザー名</label>
+        <input type="text" name="name" class="register-form__input">
+        <label class="register-form__label">メールアドレス</label>
+        <input type="email" name="email" class="register-form__input">
+        <label for="" class="register-form__label">パスワード</label>
+        <input type="password" name="password" id="" class="register-form__input">
+        <label for="" class="register-form__label">確認用パスワード</label>
+        <input type="password" name="password_confirmation" id="" class="register-form__input">
+        <div class="register-form__button">
+            <button type="submit" class="register-form__button-submit">登録する</button>
+        </div>
+    </form>
+    <a href="/login" class="transition__login-page">ログインはこちら</a>
+</div>
+@endsection
