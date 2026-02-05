3bfc7ccc68bb1ba9d9cddc791da0bf4e2a5ac8cd
keita kato <infotec62509@gmail.com>
2026-02-05 23:24:29 +0900

    商品購入画面作成（途中)

---
+++ src/resources/views/purchase.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/purchase.css') }}">
+@endsection
+
+@section('nav')
+@include('navigation')
+@endsection
+
+@section('content')
+<div class="purchase">
+    <!-- 続きここから -->
+</div>
+@endsection
