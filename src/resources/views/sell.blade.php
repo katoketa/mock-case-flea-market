bd14d27ff287006f6b30cc0e3e1c86a789dba25b
keita kato <infotec62509@gmail.com>
2026-02-19 14:10:28 +0900

    商品出品画面のスタイリング(途中)

--- src/resources/views/sell.blade.php
+++ src/resources/views/sell.blade.php
@@ -25,26 +25,29 @@
                 <input type="file" name="image" id="image" class="image-upload__input">
             </div>
         </div>
-        <div class=sell-form__detail">
-            <h3 class="sell-form__detail-header">商品の詳細</h3>
-            <label class="detail-categories__header">カテゴリー</label>
+        <div class="sell-form__detail">
+            <h3 class="sell-form__content-header">商品の詳細</h3>
+            <h4 class="detail-categories__header">カテゴリー</h4>
             <div class="detail-categories">
                 @foreach ($categories as $category)
-                <input type="checkbox" name="category[]" id="category{{ $category['id'] }}" class="detail-category">
+                <input type="checkbox" name="category[]" id="category{{ $category['id'] }}" class="detail-category__checkbox">
                 <label for="category{{ $category['id'] }}" class="detail-category__label">{{ $category['name'] }}</label>
                 @endforeach
             </div>
             <div class="detail-condition">
                 <label class="detail-condition__header">商品の状態</label>
-                <select name="condition" id="" class="detail-condition__select">
-                    @foreach ($conditions as $condition)
-                    <option value="{{ $condition['id'] }}" class="detail-condition__option">{{ $condition['name'] }}</option>
-                    @endforeach
-                </select>
+                <div class="detail-condition__select-wrapper">
+                    <select name="condition" id="" class="detail-condition__select">
+                        <option value="" class="detail-condition__option" selected hidden>選択してください</option>
+                        @foreach ($conditions as $condition)
+                        <option value="{{ $condition['id'] }}" class="detail-condition__option">{{ $condition['name'] }}</option>
+                        @endforeach
+                    </select>
+                </div>
             </div>
         </div>
         <div class="sell-form__item">
-            <h3 class="sell-form__item-header">商品名と説明</h3>
+            <h3 class="sell-form__content-header">商品名と説明</h3>
             <label class="item-name__header">商品名</label>
             <input type="text" name="name" class="item-name__input">
             <label class="item-brand__header">ブランド名</label>
